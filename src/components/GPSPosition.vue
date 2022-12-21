54b431624f69c2d2c37bc65be9d0f55196f47e75
EYKon <key.mobile@mail.ru>
2022-12-21 18:04:50 +0300

    gps

---
+++ src/components/GPSPosition.vue
@@ -0,0 +1,50 @@
+<script setup>
+	const {
+		createApp,
+		defineComponent,
+		setup,
+		ref,
+		onMounted,
+		onUnmounted
+	} = Vue
+	
+	
+	const fetchLocation = () => {
+		let watcher;
+		const geoLocation = navigator.geolocation;
+		const gpsTime = ref(Date.now());
+		const coordinates = ref({
+			accuracy: 0,
+			latitude: 0,
+			longtitude: 0,
+			altitude: 0,
+			altitudeAccuracy: 0,
+			heading: 0,
+			speed: 0
+		});
+		
+		const setPosition = (payload) => {
+			gpsTime.value = payload.timestamp
+			coordinates.value = payload.coords
+		}
+		
+		
+	}
+	
+	onMounted(() => {
+		if(geoLocation) {
+			watcher = geoLocation.watchPosition(setPosition);
+		}
+	})
+	
+	   
+	
+</script>
+
+<template>
+	<h2>GPS Position</h2>
+</template>
+
+<style scoped>
+
+</style>
