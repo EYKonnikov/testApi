f6d88a585bff30352452187b6e63e577bb9ad97b
EYKon <key.mobile@mail.ru>
2022-12-21 21:06:45 +0300

    GPS Position

--- src/components/GPSPosition.vue
+++ src/components/GPSPosition.vue
@@ -1,50 +1,71 @@
 <script setup>
-	const {
-		createApp,
-		defineComponent,
-		setup,
+	import {
 		ref,
 		onMounted,
 		onUnmounted
-	} = Vue
-	
-	
-	const fetchLocation = () => {
-		let watcher;
-		const geoLocation = navigator.geolocation;
-		const gpsTime = ref(Date.now());
-		const coordinates = ref({
-			accuracy: 0,
-			latitude: 0,
-			longtitude: 0,
-			altitude: 0,
-			altitudeAccuracy: 0,
-			heading: 0,
-			speed: 0
-		});
-		
-		const setPosition = (payload) => {
-			gpsTime.value = payload.timestamp
-			coordinates.value = payload.coords
-		}
-		
-		
+	} from 'vue'
+
+
+	let watcher;
+	const gpsTime = ref(Date.now());
+	const coordinates = ref({
+		accuracy: 0,
+		latitude: 0,
+		longtitude: 0,
+		altitude: 0,
+		altitudeAccuracy: 0,
+		heading: 0,
+		speed: 0
+	});
+
+
+	function setPosition(payload) {
+		gpsTime.value = payload.timestamp
+		coordinates.value = payload.coords
+		console.log(coordinates.value)
 	}
-	
+
 	onMounted(() => {
-		if(geoLocation) {
-			watcher = geoLocation.watchPosition(setPosition);
+		if (Geolocation) {
+			watcher = navigator.geolocation.watchPosition(setPosition)
 		}
 	})
-	
-	   
-	
+
+	onUnmounted(() => {
+		if (watcher) {
+			Geolocation.clearWatch(watcher)
+		}
+	})
 </script>
 
 <template>
-	<h2>GPS Position</h2>
+	<div class="gps">
+		<h2 class="gps__title">GPS Position</h2>
+		<div class="coordinates">
+			<p>latitude: {{coordinates.latitude}}</p>
+			<p>longtitude: {{coordinates.longitude}}</p>
+		</div>
+	</div>
+
+
 </template>
 
 <style scoped>
+	.gps,
+	.coordinates {
+		display: flex;
+		flex-direction: column;
+	}
 
+	.gps {
+		box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+		padding: 20px;
+	}
+	
+	.gps__title {
+		padding: 10px 0px;
+		
+	}
+	
+	
 </style>
